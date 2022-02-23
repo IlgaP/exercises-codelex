a1e840c220890d2d54bf84cc92d56c41d4e0b984
Ilga <ilgapalze@gmail.com>
2022-02-23 21:17:13 +0200

    Exercise 4 and 5

---
+++ src/main/java/io/codelex/typesandvariables/exercises/Exercise4.java
@@ -0,0 +1,14 @@
+package io.codelex.typesandvariables.exercises;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+
+        String name;
+        int yearOfBirth;
+
+        name = "Ilga";
+        yearOfBirth = 1992;
+
+        System.out.println("My name is " + name + " and I was born in " + yearOfBirth + ".");
+    }
+}
---
+++ src/main/java/io/codelex/typesandvariables/exercises/Exercise5.java
@@ -0,0 +1,41 @@
+package io.codelex.typesandvariables.exercises;
+import java.util.Formatter;
+public class Exercise5 {
+    public static void main(String[] args) {
+
+
+
+        String[] courses = {"English III",
+                "Precalculus",
+                "Music Theory",
+                "Biotechnology",
+                "Principles of Technology I",
+                "Latin II",
+                "AP US History",
+                "Business Computer Information Systems"
+        };
+        String[] teachers = {"Ms. Lapan",
+                "Mrs. Gideon",
+                "Mr. Davis",
+                "Ms. Palmer",
+                "Ms. Garcia",
+                "Mrs. Barnett",
+                "Ms. Johannessen",
+                "Mr. James"
+        };
+
+        Formatter fmt = new Formatter();
+        for (int i = 0; i<65; i++){
+            System.out.print("-");
+        }
+        System.out.println();
+        for(int i = 0; i<8; i++){
+            fmt.format("%3s %40s %20s\n","|" + (i+1) + "|", courses[i] +"|", teachers[i] + "|");
+
+        }
+        System.out.print(fmt);
+        for (int i = 0; i<65; i++){
+            System.out.print("-");
+        }
+    }
+}
