9588906be6cb4a6f9ee3bd526956cfa48c4609e7
Ilga <ilgapalze@gmail.com>
2022-02-25 15:57:37 +0200

    Exercise 10?

--- src/main/java/io/codelex/arithmetic/practice/Geometry.java
+++ src/main/java/io/codelex/arithmetic/practice/Geometry.java
@@ -5,18 +5,18 @@ package io.codelex.arithmetic.practice;
 import java.math.BigDecimal;
 
 class Geometry {
-    static BigDecimal areaOfCircle(BigDecimal radius) {
-        return radius.multiply(new BigDecimal(2*Math.PI));
+    static double areaOfCircle(BigDecimal radius) {
+        return radius.multiply(new BigDecimal(2*Math.PI)).doubleValue();
         //throw new NoCodeWrittenException();
     }
 
-    static BigDecimal areaOfRectangle(BigDecimal length, BigDecimal width) {
-        return length.multiply(width);
+    static double areaOfRectangle(BigDecimal length, BigDecimal width) {
+        return length.multiply(width).doubleValue();
         //throw new NoCodeWrittenException();
     }
 
-    static BigDecimal areaOfTriangle(BigDecimal base, BigDecimal h) {
-        return base.multiply(h).multiply(new BigDecimal("0.5"));
+    static double areaOfTriangle(BigDecimal base, BigDecimal h) {
+        return base.multiply(h).multiply(new BigDecimal("0.5")).doubleValue();
         //throw new NoCodeWrittenException();
     }
 }
