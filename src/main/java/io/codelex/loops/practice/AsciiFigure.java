dec76956ce199295bb0a4883b3770994bc8c1801
Ilga <ilgapalze@gmail.com>
2022-03-01 09:44:04 +0200

    Changed AsciiFigure

--- src/main/java/io/codelex/loops/practice/AsciiFigure.java
+++ src/main/java/io/codelex/loops/practice/AsciiFigure.java
@@ -1,23 +1,33 @@
 package io.codelex.loops.practice;
 
+import java.util.Scanner;
+
 public class AsciiFigure {
     public static void main(String[] args) {
 
-        for(int i = 0; i <5; i++ ){
+        int lines;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the size of figure: ");
+        lines = input.nextInt();
+        drawFigure(lines);
+    }
+    // ja lines=1 ???  ->  ////\\\\ vai ********
+
+    public static void drawFigure(int lines){
+        for(int i = 0; i <lines; i++ ){
             int stars = i * 8;
-            for(int j = 0; j <32; j++){
-                int simbolsLeft = (32 - stars) / 2;
+            for(int j = 0; j <(lines-1)*8; j++){
+                int simbolsLeft = ( (lines-1)*8- stars) / 2;
                 if(j < simbolsLeft){
                     System.out.print("/");
                 } else if(j > (simbolsLeft+stars)-1){
                     System.out.print("\\");
                 } else {
-                    System.out.print("*");
+                    System.out.print
+                            ("*");
                 }
-
             }
             System.out.println();
-
         }
     }
 }
