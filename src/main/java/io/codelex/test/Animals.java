5bccfe1441413125a2a00bbb17b998d77a3bee11
Ilga <ilgapalze@gmail.com>
2022-03-02 14:33:06 +0200

    test class

---
+++ src/main/java/io/codelex/test/Animals.java
@@ -0,0 +1,18 @@
+package io.codelex.test;
+
+public class Animals {
+    public static void main(String[] args) {
+
+        Dog myDog = new Dog();
+        myDog.setName("Reksis");
+        myDog.setAge(10);
+
+        Dog myDog2 = new Dog();
+        myDog2.setName("Arno");
+        myDog2.setAge(5);
+
+        System.out.println(myDog);
+
+
+    }
+}
