52931896ab3588cb65fa7e4d6d3e1c3923f26347
Ilga <ilgapalze@gmail.com>
2022-03-03 16:12:41 +0200

    Exercises 1-5

--- src/main/java/io/codelex/arrays/practice/Exercise5.java
+++ src/main/java/io/codelex/arrays/practice/Exercise5.java
@@ -6,24 +6,19 @@ public class Exercise5 {
     public static void main(String[] args) {
         int[] myArray = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
 
-        int indexOfElement36 = -1;
-        int indexOfElement29 = -1;
-
-        /*
-        fixme
-        for (?) {
-            
-        }
-        */
-
         //Expected output:
         //Index position of 36 is: 4
         //Index position of 29 is: 8
-        
-        /*
-        fixme
-        System.out.println("Index position of 36 is: " + indexOfElement36);
-        System.out.println("Index position of 29 is: " + indexOfElement29);
-        */
+        //fixme
+        System.out.println("Index position of 36 is: " + findIndex(myArray,36));
+        System.out.println("Index position of 29 is: " + findIndex(myArray,29));
+    }
+
+    public static int findIndex (int[] arr, int n){
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] == n){
+                return i;
+            }
+        } return -1;
     }
 }
