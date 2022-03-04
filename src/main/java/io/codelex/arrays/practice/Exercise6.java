f1db4c7404d24c513498aa394eb8a0ea6257c3e3
Ilga <ilgapalze@gmail.com>
2022-03-04 11:41:30 +0200

    Exercises 6 and TicTacToe

---
+++ src/main/java/io/codelex/arrays/practice/Exercise6.java
@@ -0,0 +1,31 @@
+package io.codelex.arrays.practice;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Exercise6 {
+    public static void main(String[] args) {
+        int size = 10;
+        int[] array1 = new int[size];
+        int[] array2 = new int[size];
+
+        Random rand = new Random();
+
+        for(int i = 0; i < size; i++){
+            array1[i] = rand.nextInt(100)+1;
+        }
+
+        for(int i = 0; i < size; i++){
+            array2[i] = array1[i];
+        }
+        array1[size-1] = -7;
+
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
+
+
+
+
+
+    }
+}
