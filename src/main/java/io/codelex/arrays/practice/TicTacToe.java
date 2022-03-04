896e1a78f9f611c80800f713acc072ed1bc76d7a
Ilga <ilgapalze@gmail.com>
2022-03-04 20:14:16 +0200

    TicTacToe

--- src/main/java/io/codelex/arrays/practice/TicTacToe.java
+++ src/main/java/io/codelex/arrays/practice/TicTacToe.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    private static char[][] board = new char[3][3];
 
     public static void main(String[] args) {
+        char[][] board = new char[3][3];
         Scanner keyboard = new Scanner(System.in);
         boolean isPlayerX = true;
-        boolean gameEmded = false;
-        initBoard();
+        boolean gameEnded = false;
+        initBoard(board);
         System.out.println("Let`s play Tic Tac Toe! :)");
 
-        while(!gameEmded){
+        while (!gameEnded) {
             System.out.println();
             displayBoard(board);
             System.out.println();
@@ -28,8 +28,8 @@ public class TicTacToe {
                 symbol = 'O';
             }
 
-            while(true) {
-                System.out.print(symbol+" - choose your location (row, column): ");
+            while (true) {
+                System.out.print(symbol + " - choose your location (row, column): ");
                 row = keyboard.nextInt();
                 col = keyboard.nextInt();
 
@@ -42,16 +42,16 @@ public class TicTacToe {
                 }
             }
             board[row][col] = symbol;
-            if(hasWon(board) == 'X'){
+            if (hasWon(board) == 'X') {
                 System.out.println("X has won");
-                gameEmded = true;
-            } else if(hasWon(board) == 'O'){
+                gameEnded = true;
+            } else if (hasWon(board) == 'O') {
                 System.out.println("O has won");
-                gameEmded = true;
+                gameEnded = true;
             } else {
-                if(isTie(board)){
+                if (isTie(board)) {
                     System.out.println("It`s a tie!");
-                    gameEmded = true;
+                    gameEnded = true;
                 } else {
                     isPlayerX = !isPlayerX;
                 }
@@ -61,53 +61,53 @@ public class TicTacToe {
         displayBoard(board);
     }
 
-    private static void initBoard() {
-        // fills up the board with blanks
+    private static void initBoard(char[][] board) {
+        // fills up the board with "-"
         for (int r = 0; r < 3; r++)
             for (int c = 0; c < 3; c++)
                 board[r][c] = '-';
     }
 
     private static void displayBoard(char[][] board) {
-        for (int r =0; r < 3; r++){
-            for(int c = 0; c < 3; c++){
-                System.out.print(" "+board[r][c]+" ");
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                System.out.print(" " + board[r][c] + " ");
             }
             System.out.println();
         }
     }
 
-    private static char hasWon (char[][] board){
+    private static char hasWon(char[][] board) {
         //row
-        for (int r = 0; r < 3; r++){
-            if(board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != '-'){
+        for (int r = 0; r < 3; r++) {
+            if (board[r][0] == board[r][1] && board[r][1] == board[r][2] && board[r][0] != '-') {
                 return board[r][0];
             }
         }
         //col
-        for (int c = 0; c < 3; c++){
-            if(board[0][c] == board[1][c] && board[1][c] == board[2][c] && board[2][c] != '-'){
+        for (int c = 0; c < 3; c++) {
+            if (board[0][c] == board[1][c] && board[1][c] == board[2][c] && board[2][c] != '-') {
                 return board[2][c];
             }
         }
         //diagonals
-        if(board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-'){
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[0][0] != '-') {
             return board[0][0];
         }
-        if(board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[0][2] != '–'){
+        if (board[2][0] == board[1][1] && board[1][1] == board[0][2] && board[0][2] != '–') {
             return board[0][2];
         }
         return '-';
     }
 
-    private static boolean isTie(char[][] board){
-        for(int r = 0; r < 3; r++){
-            for(int c = 0; c < 3; c++){
-                if(board[r][c] == '-'){
+    private static boolean isTie(char[][] board) {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-') {
                     return false;
                 }
             }
         }
-        return  true;
+        return true;
     }
 }
