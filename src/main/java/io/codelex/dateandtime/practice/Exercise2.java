03c604452c94c5dff0e5eb740230caeba19290ae
Ilga <ilgapalze@gmail.com>
2022-03-15 14:11:04 +0200

    Ex. #2, #3

--- src/main/java/io/codelex/dateandtime/practice/Exercise2.java
+++ src/main/java/io/codelex/dateandtime/practice/Exercise2.java
@@ -1,19 +1,24 @@
 package io.codelex.dateandtime.practice;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Exercise2 {
     public static void main(String[] args) {
 
-        LocalDate serverLaunched = LocalDate.of(2022, 1, 1);
         Scanner input = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("Enter server launch time(for ex. 01/01/2022): ");
+        String launch = input.nextLine();
+        LocalDate serverLaunched = LocalDate.parse(launch, formatter);
         System.out.println("To see update times, enter -");
         System.out.print("Year: ");
         int year = input.nextInt();
         System.out.print("Month: ");
         int month = input.nextInt();
-
+        System.out.println();
+        System.out.println("Server update:");
         Exercise2.getUpdateDates(serverLaunched, year, month);
 
     }
