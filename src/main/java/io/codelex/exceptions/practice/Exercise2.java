65bc4df657da13adf731d334ea8491b91ca9e18c
Ilga <ilgapalze@gmail.com>
2022-03-23 11:12:47 +0200

    exercises

---
+++ src/main/java/io/codelex/exceptions/practice/Exercise2.java
@@ -0,0 +1,40 @@
+package io.codelex.exceptions.practice;
+
+import java.util.Arrays;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        Exercise2 exercise2 = new Exercise2();
+
+        exercise2.methodA();
+    }
+
+    public void methodA() throws ArithmeticException {
+        try {
+            methodB();
+        } catch (ArithmeticException e) {
+            System.out.println(Arrays.toString(new Throwable().getStackTrace()));
+        }
+    }
+
+    public void methodB() throws ArithmeticException {
+        try {
+            methodC();
+        } catch (ArithmeticException e) {
+            System.out.println(Arrays.toString(new Throwable().getStackTrace()));
+            throw new ArithmeticException("/ by zero");
+        }
+    }
+
+    public void methodC() throws ArithmeticException {
+        int numerator = 12;
+        int divisor = 0;
+        try {
+            int result = numerator / divisor;
+        } catch (ArithmeticException e) {
+            System.out.println(Arrays.toString(new Throwable().getStackTrace()));
+            throw new ArithmeticException("/ by zero");
+        }
+    }
+}
