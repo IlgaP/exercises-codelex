a355bca78ad907a370f0328de47cc964fbbbcdb5
Ilga <ilgapalze@gmail.com>
2022-03-30 16:25:54 +0300

    labojumi

--- src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
+++ src/main/java/io/codelex/dateandtime/practice/DatePeriod.java
@@ -14,12 +14,20 @@ public class DatePeriod {
     public DatePeriod intersection(DatePeriod date) {
         DatePeriod overlap1 = partiallyOverlap(date);
         DatePeriod overlap2 = completelyOverlap(date);
-        DatePeriod overlap3 = notOverlap(date);
+        DatePeriod overlap3 = oneIncludeOther(date);
+        DatePeriod overlap4 = oneIncludeOther2(date);
+        DatePeriod overlap5 = notOverlap(date);
         if (overlap1 != null) {
             return overlap1;
         } else if (overlap2 != null) {
             return overlap2;
-        } else return overlap3;
+        } else if (overlap3 != null) {
+            return overlap3;
+        } else if (overlap4 != null) {
+            return overlap4;
+        } else {
+            return overlap5;
+        }
     }
 
     private DatePeriod partiallyOverlap(DatePeriod date) {
@@ -43,6 +51,20 @@ public class DatePeriod {
         return null;
     }
 
+    private DatePeriod oneIncludeOther(DatePeriod date) {
+        if ((date.start.isBefore(start) || date.start.equals(start)) && ((end.isBefore(date.end)) || (end.equals(date.end)))) {
+            return new DatePeriod(start, end);
+        }
+        return null;
+    }
+
+    private DatePeriod oneIncludeOther2(DatePeriod date) {
+        if (start.isBefore(date.start) && date.end.isBefore(end)) {
+            return new DatePeriod(date.start, date.end);
+        }
+        return null;
+    }
+    
     @Override
     public String toString() {
         return "Start = " + start +
